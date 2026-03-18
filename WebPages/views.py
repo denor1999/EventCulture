1f25768d10304ebb1643dfc200861d66f4d23ef7
denor1999 <ozhgixinv@list.ru>
2026-03-18 15:59:16 +0300

    Create  authenticate

--- WebPages/views.py
+++ WebPages/views.py
@@ -1,13 +1,41 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
 
-def login(request):
-    return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
 
-def registration(request):
-    return render(request, 'registration.html')
+        if User is not None:
+            login(request, user)
+            messages.success(request, 'Успешный вход!')
+        else:
+            messages.error(request, 'Неверный догин или пароль!')
+
+    return redirect('home')
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Регистрация прошла успешно!')
+        else:
+            for error in form.errors.values():
+                messages.error(request, error)
+
+    return redirect('home')
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, 'Вы вышли из системы')
+    return redirect('home')
 # Create your views here.
